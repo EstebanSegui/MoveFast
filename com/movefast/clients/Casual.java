78ffe99ebe78dcb36db2cb5ddd5ec1260ee5d419
ayoub <ayoubbouyssfdaouan@iespaucasesnoves.cat>
2018-05-11 18:53:58 +0200

    Primer Commit

---
+++ com/movefast/clients/Casual.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.movefast.clients;
+
+/**
+ *
+ * @author emilio
+ */
+public class Casual extends Client{
+    
+    private double pagament;
+    
+    
+    
+    
+    
+}
---
+++ com/movefast/clients/Client.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.movefast.clients;
+
+/**
+ *
+ * @author emilio
+ */
+public class Client {
+    
+    private String dni;
+    private String nom;
+    private String llinatge;
+    private int telefon;
+    private String numComptebancari;
+    
+}
